26ca9b5cc1c2f7d446a0da5f67af1a282333ca5b
Greco Marques <greco92003@gmail.com>
2025-02-18 17:38:07 -0300

    mudei para app para afiliados

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ export default function CouponGenerator() {
 
   const generateCouponLink = () => {
     if (!name || !discount) return;
-    const utmMedium = `${name}-${discount}OFF`;
-    const url = `https://hudlab.com.br/cadastro-amostra-digital?utm_source=Cupom&utm_medium=${utmMedium}&utm_id=Cupom-de-desconto`;
+    const utmMedium = `${name}-${discount}%-DE-COMISSÃO`;
+    const url = `https://hudlab.com.br/cadastro-amostra-digital?utm_source=Afiliados&utm_medium=${utmMedium}&utm_id=Link-de-afiliado`;
     setGeneratedLink(url);
   };
 
@@ -26,28 +26,36 @@ export default function CouponGenerator() {
     <div className="flex flex-col items-center p-6 gap-4 bg-transparent">
       <Card className="w-full max-w-md p-4">
         <CardContent className="flex flex-col gap-4 items-center">
-        <img src={logo} alt="Logo" className="w-auto h-28 m-5" />
+          <img src={logo} alt="Logo" className="w-auto h-28 m-5" />
           <label>@ Rede Social</label>
           <Input
-            placeholder="Digite o nome do dono do cupom"
+            placeholder="Digite o @ do afiliado"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <label>Porcentagem do desconto</label>
+          <label>Porcentagem de Comissão</label>
           <Input
             type="number"
-            placeholder="Digite a porcentagem do desconto"
+            placeholder="Digite a porcentagem da comissão"
             value={discount}
             onChange={(e) => setDiscount(e.target.value)}
           />
-          <Button className="mt-5" onClick={generateCouponLink}>Gerar Cupom</Button>
+          <Button className="mt-5" onClick={generateCouponLink}>
+            Gerar Link
+          </Button>
         </CardContent>
       </Card>
       {generatedLink && (
         <Card className="w-full max-w-md p-4 flex flex-col items-center gap-2">
           <CardContent className="flex flex-col items-center gap-2">
-            <p className="break-all text-center text-cyan-400">{generatedLink}</p>
-            <Button className="mt-5" onClick={copyToClipboard} variant="outline">
+            <p className="break-all text-center text-cyan-400">
+              {generatedLink}
+            </p>
+            <Button
+              className="mt-5"
+              onClick={copyToClipboard}
+              variant="outline"
+            >
               <ClipboardCopy className="w-4 h-4 mr-2" /> Copiar Link
             </Button>
           </CardContent>
